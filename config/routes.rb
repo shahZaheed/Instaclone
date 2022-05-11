57fa8e8929d48da94f1a43d4e857302b475b2bae
shahZaheed <zahidshah99599@gmail.com>
2022-05-11 18:50:34 +0530

    auth added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   #get 'pages/home'
   root 'pages#home'
-  devise_for :users
+  #devise_for :users
+  devise_for :users do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
